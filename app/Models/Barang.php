c8ef5cf881403b11f6c77c914adcb23469507fa8
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-04-15 20:44:28 +0700

    update model Barang

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'kode_barang',
+        'nama_barang',
+        'jumlah',
+        'kategori'
+    ];
+
+    public function histoy()
+    {
+        return $this->hasMany(History::class, 'id_barang', 'id');
+    }
 }
