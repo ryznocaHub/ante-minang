e9dcb3949cc2b8a2d5cb2b786235a354cbf2b86b
ryznocaHub <fikrihalim27@gmail.com>
2021-04-15 21:16:07 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ManagementBarangController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,13 +14,20 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+| 
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => 'Admin'], function () {
+    Route::resource('users',         UserController::class)->except(['edit']);
+});
+
+Route::group(['middleware' => 'Pegawai'], function () {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('history/barang/masuk',  [ManagementBarangController::class, 'masuk'])->name('history.masuk');
+    Route::get('history/barang/keluar',  [ManagementBarangController::class, 'keluar'])->name('history.keluar');
+    Route::resource('manajemen',    ManagementBarangController::class);
+    Route::resource('users',        UserController::class)->only(['show', 'update']);
+});
+
