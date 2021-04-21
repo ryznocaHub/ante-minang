6e7bc39fecec305270ea7e5002a6d775c02570ec
wdftr <ikhbal.madrid17@gmail.com>
2021-04-21 13:05:58 +0700

    add history_bahan_bakus table

---
+++ database/migrations/2021_04_21_051759_create_history_bahan_bakus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoryBahanBakusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('history_bahan_bakus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('bahan_baku_id')->references('id')->on('bahan_bakus')->onDelete('set null');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->integer('jumlah');
+            $table->text('keterangan');
+            $table->string('kategori');
+            $table->timestamp('tanggal')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('history_bahan_bakus');
+    }
+}
