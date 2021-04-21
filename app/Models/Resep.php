de35c59b50fc5da3b24fa91b78b969b501a77192
wdftr <ikhbal.madrid17@gmail.com>
2021-04-21 13:04:58 +0700

    update Resep model

--- app/Models/Resep.php
+++ app/Models/Resep.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Resep extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'produk_id',
+        'bahan_baku_id',
+        'jumlah'
+    ];
+
+    public function produk()
+    {
+        return $this->belongsTo(Produk::class, 'produk_id', 'id');
+    }
+
+    public function bahanbaku()
+    {
+        return $this->belongsTo(BahanBaku::class, 'bahan_baku_id', 'id');
+    }
 }
