8cbab4d0d443b111603c8d9b8db0cf7edb956b56
wdftr <ikhbal.madrid17@gmail.com>
2021-04-21 13:03:38 +0700

    update BahanBaku model

--- app/Models/BahanBaku.php
+++ app/Models/BahanBaku.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class BahanBaku extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'kode',
+        'nama',
+        'jumlah',
+        'satuan'
+    ];
+
+    public function history()
+    {
+        return $this->hasMany(HistoryBahanBaku::class, 'bahan_baku_id', 'id');
+    }
+
+    public function historymanagement()
+    {
+        return $this->hasMany(HistoryManagementBahanBaku::class, 'bahan_baku_id', 'id');
+    }
+
+    public function resep()
+    {
+        return $this->hasMany(Resep::class, 'bahan_baku_id', 'id');
+    }
 }
