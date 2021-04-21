04f49b997503934d9c9c4805baee124a1d87b11b
wdftr <ikhbal.madrid17@gmail.com>
2021-04-21 13:04:45 +0700

    update Produk model

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Produk extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    public $fillable = [
+        'kode',
+        'nama',
+        'jumlah',
+        'satuan'
+    ];
+
+    public function history()
+    {
+        return $this->hasMany(HistoryProduk::class, 'produk_id', 'id');
+    }
+
+    public function historymanagement()
+    {
+        return $this->hasMany(HistoryManagementProduk::class, 'produk_id', 'id');
+    }
+
+    public function resep()
+    {
+        return $this->hasMany(Resep::class, 'produk_id', 'id');
+    }
 }
